21c7e624b7ff2baf67049d8ee52ab97ecd692dd8
jonemo <jonemo@stud.ntnu.no>
2020-09-22 21:42:56 +0200

    started text on board

--- src/GameCenter/board.java
+++ src/GameCenter/board.java
@@ -10,17 +10,36 @@ public class board {
                     {13,14,15,16,17,18},
                     {12,11,10,9,8,7},
                     {1,2,3,4,5,6}};
+    int[][] map = {{0,0,0,0,0,2},
+                    {0,0,0,0,0,0},
+                    {0,0,0,0,0,0},
+                    {0,0,0,0,0,0},
+                    {0,0,0,0,0,0},
+                    {1,0,0,0,0,0}};
 
     public void createBoard(JPanel mainPanel) {
         JPanel fullboard = new JPanel();
         fullboard.setLayout(new GridLayout(6,6));
         JButton[][] Bboard = new JButton[6][6];
+        String text = "";
 
         for (int i = 0; i < 6; i++) {
             for(int j = 0; j<6; j++){
-                Bboard[i][j] = new JButton(String.valueOf(index[i][j]));
+                text = "<html>"+index[i][j];
+                switch(map[i][j]){ // TODO: switch to listindexes. and internationalisation
+                    case 1:
+                        text += "<br>Start";
+                        break;
+                    case 2:
+                        text += "<br>Finish";
+                        break;
+                }
+                text+= "</html>";
+
+                Bboard[i][j] = new JButton(text);
                 Bboard[i][j].setPreferredSize(new Dimension(50,50));
                 fullboard.add(Bboard[i][j]);
+
             }
         }
 
