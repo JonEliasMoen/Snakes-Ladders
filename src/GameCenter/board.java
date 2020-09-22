5b172c8b64ae03496a0743bc3aedb0972e819809
jonemo <jonemo@stud.ntnu.no>
2020-09-22 21:16:34 +0200

    index for board

--- src/GameCenter/board.java
+++ src/GameCenter/board.java
@@ -4,21 +4,27 @@ import javax.swing.*;
 import java.awt.*;
 
 public class board {
-
+    int[][] index = {{31,32,33,34,35,36},
+                    {25,26,27,28,29,30},
+                    {24,23,22,21,20,19},
+                    {13,14,15,16,17,18},
+                    {12,11,10,9,8,7},
+                    {1,2,3,4,5,6}};
 
     public void createBoard(JPanel mainPanel) {
-        JPanel[] fullboard = new JPanel[6];
+        JPanel fullboard = new JPanel();
+        fullboard.setLayout(new GridLayout(6,6));
         JButton[][] Bboard = new JButton[6][6];
 
         for (int i = 0; i < 6; i++) {
-            fullboard[i] = new JPanel();
-            for (int j = 0; j < 6; j++) {
-                Bboard[i][j] = new JButton(String.valueOf((5 - i) * 6 + (j + 1)));
-                Bboard[i][j].setPreferredSize(new Dimension(50,30));
-                fullboard[i].add(Bboard[i][j]);
+            for(int j = 0; j<6; j++){
+                Bboard[i][j] = new JButton(String.valueOf(index[i][j]));
+                Bboard[i][j].setPreferredSize(new Dimension(50,50));
+                fullboard.add(Bboard[i][j]);
             }
-            mainPanel.add(fullboard[i]);
         }
+
+        mainPanel.add(fullboard);
     }
 
     public void updateBoard(JFrame main) {
