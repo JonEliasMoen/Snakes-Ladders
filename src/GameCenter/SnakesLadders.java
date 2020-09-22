eafb1f84e40991aba0683763810ce31b9027c06f
jonemo <jonemo@stud.ntnu.no>
2020-09-22 19:49:19 +0200

    basic layout

--- src/GameCenter/SnakesLadders.java
+++ src/GameCenter/SnakesLadders.java
@@ -1,12 +1,10 @@
 package GameCenter;
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class SnakesLadders {
-    public int boardButtonSize = 10; // board button
-    public int[] wSize = {400, 400}; // hver celle er en classe eller struct?
+    public int[] wSize = {400, 450}; // hver celle er en classe eller struct?
     public double[][] board = {{0, 0, 0, 0, 0, -3}, // -1.2 finish, -1.1 start
             {0, 0, 0, 0, 0, 0}, // 1-n player n
             {0, 0, 0, 0, 0, 0}, //
@@ -21,16 +19,18 @@ public class SnakesLadders {
     final public diceHandler dh = new diceHandler();
     final public board mainBoard = new board();
 
+
     public SnakesLadders() {
+        // mainframe
         JFrame mainframe = new JFrame("Snakes&Ladders");
         mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        mainframe.setSize(400, 400);   // set window size
+        mainframe.setSize(wSize[0], wSize[1]);   // set window size
         mainframe.setLocationRelativeTo(null); // middle of screen
         mainframe.setLayout(null); // no layout manager
         mainframe.setResizable(false); // not resizable
         mainframe.setVisible(true); // is visable
-        mainframe.setLayout(new BorderLayout());
+        mainframe.setLayout(null);
 
         //toolbar
         Container contentPane = mainframe.getContentPane();
@@ -41,14 +41,29 @@ public class SnakesLadders {
 
         tb.add(tbStart); tb.add(tbReset); tb.add(tbExit);
         tb.setFloatable(false);
-        mainframe.add(tb, BorderLayout.NORTH);
+        tb.setBounds(0,0,400,20);
+        mainframe.add(tb);
 
-        //mainframe.add(tb);
+        // mainPanel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        // board
+        JPanel[] fullboard = new JPanel[6];
+        JButton[][] Bboard = new JButton[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            fullboard[i] = new JPanel();
+            for (int j = 0; j < 6; j++) {
+                Bboard[i][j] = new JButton(String.valueOf((5 - i) * 6 + (j + 1)));
+                Bboard[i][j].setPreferredSize(new Dimension(50,30));
+                fullboard[i].add(Bboard[i][j]);
+            }
+            mainPanel.add(fullboard[i]);
+        }
+
 
         // dice rolls
-        int dOffset = 25;
-        int diceSize = 30;
-        int diceY = 100;
         JPanel dicePanel = new JPanel();
         JTextField d1 = new JTextField("D1");
         JTextField d2 = new JTextField("D2");
@@ -56,47 +71,29 @@ public class SnakesLadders {
         sum.setBorder(javax.swing.BorderFactory.createEmptyBorder());
         JButton roll = new JButton("roll");
 
-
-        d1.setBounds(wSize[0] / 4 - dOffset, diceY, diceSize, diceSize);
-        d2.setBounds(wSize[0] / 2 - dOffset, diceY, diceSize, diceSize);
-        sum.setBounds(wSize[0] / 2 + 10, diceY, 60, 30);
-        roll.setBounds(3 * wSize[0] / 4 - dOffset, diceY, 75, diceSize);
-
-        d1.setEditable(false);
-        d2.setEditable(false);
-        sum.setEditable(false);
-        dicePanel.add(d1);
-        dicePanel.add(d2);
-        dicePanel.add(sum);
-
+        d1.setEditable(false); d2.setEditable(false); sum.setEditable(false);
+        dicePanel.add(d1); dicePanel.add(d2); dicePanel.add(sum);
         dicePanel.add(roll);
-        mainframe.add(dicePanel);
 
-        mainBoard.createBoard(mainframe);
+        mainPanel.add(dicePanel);
 
-        JPanel fullboard = new JPanel(new GridLayout(6,1));
-        JButton[][] Bboard = new JButton[6][6];
-        JPanel[] bPanel = new JPanel[6];
-
-        for (int i = 0; i < 6; i++) {
-            bPanel[i] = new JPanel();
-            for (int j = 0; j < 6; j++) {
-                Bboard[i][j] = new JButton(String.valueOf((5 - i) * 6 + (j + 1)));
-                bPanel[i].add(Bboard[i][j]);
-            }
-            fullboard.add(bPanel[i]);
-        }
-        mainframe.add(fullboard, BorderLayout.AFTER_LAST_LINE);
         // p1/p2 turn
-        JTextArea turnInfo = new JTextArea("player x");
+        JPanel turnPanel = new JPanel();
+        JTextField turnInfo = new JTextField("Player turn:     ");
+        turnInfo.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        turnInfo.setBounds(0,0,20,30);
+        turnInfo.setEditable(false);
+        turnPanel.add(turnInfo);
+        mainPanel.add(turnPanel);
 
-        //mainframe.add(turnInfo);
+        // placement of mainPanel
+        mainPanel.setBounds(0,20,wSize[0],wSize[0]-20);
+        mainframe.add(mainPanel);
 
         // actionlisteners
         tbExit.addActionListener(e -> {
             mainframe.dispose();
         });
 
-        //mainframe.pack();
     }
 }
